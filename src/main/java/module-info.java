35d4b638d5834b9661ab01ff95d0bc959c155038
Bernhard Fisseni <bernhard.fisseni@mail.de>
2021-12-11 13:56:29 +0100

    dependency update

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,7 @@
 open module org.korpora.useful {
     requires javax.servlet.api;
     requires java.xml;
-    requires jdom2;
+    requires org.jdom2;
     requires org.apache.commons.lang3;
     requires com.fasterxml.jackson.core;
     requires org.apache.commons.io;
