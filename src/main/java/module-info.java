ae9f811cd61bd56aa6a2b235bd5bc7502f8e1cbd
Bernhard Fisseni <bernhard.fisseni@mail.de>
2023-03-12 12:49:32 +0100

    fix transitive dependency

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -8,7 +8,7 @@ open module org.korpora.useful {
     requires com.fasterxml.jackson.core;
     requires org.apache.commons.io;
     requires com.fasterxml.jackson.databind;
-    requires jakarta.servlet;
+    requires transitive jakarta.servlet;
     requires org.slf4j;
     exports org.korpora.useful;
 }
